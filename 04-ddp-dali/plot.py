7f53d559e6fcbfae18abaf95600414d918afbe8a
Lucas Yunkyu Lee <lucas@yklcs.com>
2025-10-07 14:23:36 +0900

    Update plotting script

--- 04-ddp-dali/plot.py
+++ 04-ddp-dali/plot.py
@@ -5,14 +5,21 @@ import numpy as np
 matplotlib.use("pgf")
 plt.rcParams.update({"font.family": "serif", "pgf.rcfonts": False})
 
-data = {
-    1: {"DP": 48.508, "DDP": 47.083, "DDP+DALI": 22.700},
-    2: {"DP": 42.485, "DDP": 24.431, "DDP+DALI": 11.891},
+data_ours = {
+    1: {"DP": 47.905, "DDP": 46.740, "DDP+DALI": 22.937},
+    2: {"DP": 42.863, "DDP": 26.947, "DDP+DALI": 13.018},
 }
 
 
-def main():
-    fig, ax = plt.subplots(1, 1, figsize=(6, 3), layout="constrained")
+data_baseline = {
+    1: {"DP": 30.398, "DDP": 32.615, "DDP+DALI": 10.646},
+    2: {"DP": 32.169, "DDP": 17.647, "DDP+DALI": 7.195},
+    4: {"DP": 34.362, "DDP": 13.558, "DDP+DALI": 6.722},
+}
+
+
+def main(data):
+    fig, ax = plt.subplots(1, 1, figsize=(4, 2.5), layout="constrained")
 
     x = np.arange(3)
     width = 0.25
@@ -24,20 +31,19 @@ def main():
             x + offset,
             list(y.values()),
             width,
-            color=("tab:blue", 0.5 + 0.5 * i / (len(data) - 1)),
+            color=("tab:blue", 0.2 + 0.8 * i / (len(data) - 1)),
             label=label,
             edgecolor="black",
         )
-        ax.bar_label(rects, padding=3)
+        ax.bar_label(rects, padding=3, fmt="{:.1f}")
 
-    ax.set_ylabel("Training Time (s)")
-    ax.set_title("Training Time, 1 Epoch (Our Measurements)")
-    ax.set_xticks(x + 0.5 * width + sep / 2, data[1].keys())
-    ax.legend(title="GPU Count", ncols=2)
+    ax.set_ylabel("Training Time (s) $\\downarrow$")
+    ax.set_xticks(x + (width + sep) / 2, data[1].keys())
+    ax.legend(title="GPU Count", ncols=1)
     ax.set_ylim(0, 55)
 
     fig.savefig("plot.pdf", backend="pgf", dpi=300)
 
 
 if __name__ == "__main__":
-    main()
+    main(data_ours)
